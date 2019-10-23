ac77788d0768d78f70f3922dc0c30b3248d9abca
shinz <sethhinz@me.com>
2019-10-22 20:47:09 -0700

    Fixed likely bug in test 1?

--- src/test/java/cpen221/mp2/GraphTest.java
+++ src/test/java/cpen221/mp2/GraphTest.java
@@ -30,7 +30,7 @@ public class GraphTest {
         g.addEdge(e3);
 
         assertEquals(e2, g.getEdge(v2, v3));
-        assertEquals(21, g.pathLength(shortestPath(v3, v4)));
+        assertEquals(21, g.pathLength(g.shortestPath(v3, v4)));
     }
 
 }
