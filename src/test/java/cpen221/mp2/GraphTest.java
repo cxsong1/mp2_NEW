eeac5e48b5ddd2e336e43cc4a3fb786863c86446
cxsong1 <csca12.cs@gmail.com>
2019-10-25 08:43:33 -0700

    Make graph functions for testing

--- src/test/java/cpen221/mp2/GraphTest.java
+++ src/test/java/cpen221/mp2/GraphTest.java
@@ -8,7 +8,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class GraphTest {
-    public void createGraph() {
+
+    //Creates a graph to be used for tests
+    public Graph<Vertex, Edge<Vertex>> createGraph1() {
         Vertex v1 = new Vertex(1, "A");
         Vertex v2 = new Vertex(2, "B");
         Vertex v3 = new Vertex(3, "C");
@@ -28,6 +30,37 @@ public class GraphTest {
         g.addEdge(e3);
 
         return g;
+    }
+
+    public Graph<Vertex, Edge<Vertex>> createGraph2() {
+        Vertex v1 = new Vertex(1, "A");
+        Vertex v2 = new Vertex(2, "B");
+        Vertex v3 = new Vertex(3, "C");
+        Vertex v4 = new Vertex(4, "D");
+        Vertex v5 = new Vertex(5, "E");
+        Vertex v6 = new Vertex(6, "F");
+
+        Edge<Vertex> e1 = new Edge<>(v1, v2, 5);
+        Edge<Vertex> e2 = new Edge<>(v2, v3, 7);
+        Edge<Vertex> e3 = new Edge<>(v1, v4, 9);
+        Edge<Vertex> e4 = new Edge<>(v1, v5, 6);
+        Edge<Vertex> e5 = new Edge<>(v2, v6, 3);
+
+        Graph<Vertex, Edge<Vertex>> g2 = new Graph<>();
+        g2.addVertex(v1);
+        g2.addVertex(v2);
+        g2.addVertex(v3);
+        g2.addVertex(v4);
+        g2.addVertex(v5);
+        g2.addVertex(v6);
+        g2.remove(v6);
+        g2.addEdge(e1);
+        g2.addEdge(e2);
+        g2.addEdge(e3);
+
+        return g2;
+    }
+
 
 //        assertEquals(e2, g.getEdge(v2, v3));
 //        assertEquals(21, g.pathLength(g.shortestPath(v3, v4)));
@@ -35,4 +68,3 @@ public class GraphTest {
 //        assertEquals(g.edgeLength(v2, v3), 7);
     }
 
-}
